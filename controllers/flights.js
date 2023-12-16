54799ae4a5203253d2ee053eaac191191b06da93
Paul K <pkerschieter@gmail.com>
2023-12-15 20:48:30 -0600

    fixed broken add flight on error

--- controllers/flights.js
+++ controllers/flights.js
@@ -36,6 +36,6 @@ async function create(req, res) {
     } catch (err) {
       // Typically some sort of validation error
         console.log(err);
-        res.render('flights/new', { errorMsg: err.message });
+        res.render('flights/new', { title: 'New Flights', errorMsg: err.message });
     }
 }
