8e4ce95e939109ecd05c9e534313faeaea5dff55
Paul K <pkerschieter@gmail.com>
2023-12-15 20:54:01 -0600

    fixed error with show function

--- controllers/flights.js
+++ controllers/flights.js
@@ -13,8 +13,8 @@ async function index(req, res) {
 }
 
 async function show(req, res) {
-    const flights = await FlightfindById(req.params.id);
-    res.render('flights/show', { title: 'Flight Detail', flights });  
+    const flight = await Flight.findById(req.params.id);
+    res.render('flights/show', { title: 'Flight Detail', flight });  
 }
 
 function newFlight(req, res) {
